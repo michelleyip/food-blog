4708143fa5bbbee04d6039a8e4e85245baa190bb
MichelleY <michelle@macbook.local>
2021-01-11 10:02:07 +0000

    create initial setup

---
+++ src/components/parts/Navigation.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+class Navigation extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+      };
+    }
+
+
+
+
+
+    render() {
+
+    }
+    return ();
+
+}
+
+
+
+
+
+export default Navigation;
